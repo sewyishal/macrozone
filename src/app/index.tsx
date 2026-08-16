6ec1a797d1c420e0aa18f52072475fae38fd44dd
sewyishal <sewyishalx@gmail.com>
2026-08-16 15:33:31 +0300

    added homeheader

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import { globalStyles } from '../../styles/global';
+import HomeHeader from './HomeHeader';
 
 export default function HomeScreen() {
   return (
-    <View style={globalStyles.container}>
+    <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
-      <Text style={styles.date}>Monday, March 16</Text>
-    </View>
+      <HomeHeader/>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  date: {
-    fontSize: 14,
-    color: '#a0a0b0',
-    marginTop: 4,
-    marginBottom: 30,
-  },
-});
